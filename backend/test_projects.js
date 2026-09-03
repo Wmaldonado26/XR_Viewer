b46d8fb190b20bc0a50d6fec858d341be11560c9
wmaldonado <wmaldonado@cotecmar.com>
2026-09-03 07:39:03 -0500

    chore: clean up project by removing obsolete scripts, docs, and untracking build folders

--- backend/test_projects.js
+++
@@ -1,6 +0,0 @@
-const Database = require('better-sqlite3');
-const db = new Database('prisma/cotecmar.db');
-
-const rows = db.prepare("SELECT * FROM projects WHERE id = 'businu'").all();
-console.log(rows);
-db.close();
